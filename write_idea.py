42a70357010617c457bd730bad307bc2c43ed76f
Bobby <steinbach.rj@gmail.com>
2014-09-26 12:26:04 -0400

    added demo file showing output. Added repeat attempts to try and get definitions of new words if no definitions were retrieved after first run

--- write_idea.py
+++ write_idea.py
@@ -7,6 +7,7 @@ import ipdb
 
 #in the future, we can have this passed in to the program from command line
 MAX_TO_CHOOSE = 3
+MAX_ATTEMPTS_TO_GET_DEFINITIONS = 3
 
 def get_random_words(max_to_choose):
     """
@@ -32,16 +33,26 @@ def get_definition_from_merriam(word):
     api_url = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/{word}?key={key}'.format(word=word, key=api_key)
     resp = requests.get(api_url)
 
+    definition = None
     try:
         soup = BeautifulSoup(resp.content)
-        return soup.dt
+        definition = soup.dt.find(text=True).strip(":")
     except Exception:
-        return None
+        pass
+
+    return definition
 
 
 print "\n\nWords Of The Day:"
 print ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
-for word in get_random_words(MAX_TO_CHOOSE):
-    definition = get_definition_from_merriam(word)
-    if definition:
-        print "{word}: {defn}\n".format(word=word, defn=definition)
+
+definitions = {}
+i = 0
+while len(definitions) < 1 and i < MAX_ATTEMPTS_TO_GET_DEFINITIONS:
+    for word in get_random_words(MAX_TO_CHOOSE):
+        definition = get_definition_from_merriam(word)
+        if definition:
+            definitions[word] = definition
+    i += 1
+
+print "\n".join(["{word}: {defn}".format(word=word, defn=definition) for word, definition in definitions.items()])
